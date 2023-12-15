7308ce0606d907187ae18a5fb20bec8c7c7835f4
umair <umairalism@gmail.com>
2023-12-15 20:33:36 +0500

    added all the basic files

---
+++ main.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+# List of fruits
+fruits = ['Applee', 'Guavaa', 'Orange', 'Grapes', 'Watermelon']
+
+# Corresponding quantities or counts
+quantities = [25, 18, 30, 15, 10]
+
+# Create a bar chart
+plt.bar(fruits, quantities, color='skyblue')
+
+# Add labels and title
+plt.xlabel('Fruits')
+plt.ylabel('Quantity')
+plt.title('Fruit Quantities')
+
+# Display the bar chart
+plt.show()
+
+# Save the graph as an image file (e.g., PNG)
+plt.savefig('fruits.png')
